0fc4f098ef438ecaeadfcd3cc52d357f7ff98a0b
Venkat Gudipati <98337647+gvr9094@users.noreply.github.com>
2022-10-16 13:44:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import emojiList from './Components/emojiList';
+import Main from './Components/Main';
+import Nav from './Components/Nav';
+
+function App() {
+  return (
+    <div className='App'>
+    <Nav/>
+    <Main/>
+    </div>
+  );
+}
+
+export default App;
